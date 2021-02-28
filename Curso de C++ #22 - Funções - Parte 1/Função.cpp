6b996462ca1a477e2f32e0e80e0babf27e8a42ce
driano220 <79794400+driano220@users.noreply.github.com>
2021-02-28 19:50:37 -0300

    Exemplo

---
+++ Curso de C++ #22 - Funções - Parte 1/Função.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+
+void texto();
+void soma (int n1, int n2);
+
+int main ()
+{
+
+    soma(15,5);
+    texto();
+    return 0;
+  
+}
+
+void texto()
+{
+
+ cout<<"Adriano Martins "<<"\n";
+
+
+}
+
+void soma (int n1, int n2)
+{
+ cout<<"Soma dos valores: "<<n1+n2<<"\n";
+
+
+}
