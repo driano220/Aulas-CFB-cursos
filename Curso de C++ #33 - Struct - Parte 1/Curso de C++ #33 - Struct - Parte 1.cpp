077199d424495029e7c0d23d321f3a9e073215b7
driano220 <79794400+driano220@users.noreply.github.com>
2021-03-01 11:44:12 -0300

    Com duas structs

--- Curso de C++ #33 - Struct - Parte 1/Curso de C++ #33 - Struct - Parte 1.cpp
+++ Curso de C++ #33 - Struct - Parte 1/Curso de C++ #33 - Struct - Parte 1.cpp
@@ -17,16 +17,32 @@ struct Carro
 
  }; 
 
+ struct Console 
+ {
+    string nome_console;
+    float preco;
+    int ano_lancamento;
+    int n_jogos;
+
+
+
+
+
+ };
+
 
 int main ()
 {
     setlocale(LC_ALL, "portuguese");
 
-
+  Console ps4; 
 
   Carro car_1;
+  
+  
   cout<<"Qual e a potencia do carro: ";
   cin>>car_1.pot;
+  
   cout<<"\n\n";
   
   cout<<"Entao, a potencia do carro e :"<<car_1. pot<<"\n\n";   
@@ -35,6 +51,15 @@ int main ()
   cin>>car_1.nome;
 
   cout<<" O nome do carro: "<<car_1.nome<<"\n\n";
+
+    cout<<"Digite o nome do console"<<"\n\n";
+    cin>>ps4.nome_console;
+
+    cout<<"Digite o valor do console";
+    cin>>ps4.preco;
+
+    cout<<"Nome do console e: "<<ps4.nome_console<<", e o preco e: "<<ps4.preco;
+
 
 
 
